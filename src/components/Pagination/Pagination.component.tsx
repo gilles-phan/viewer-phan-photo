28430758662ae8727dfeb85584222b3947704637
Gilles PHAN <gilles.phan@sii.fr>
2024-05-03 22:56:58 +0200

    - fix pagination

--- src/components/Pagination/Pagination.component.tsx
+++ src/components/Pagination/Pagination.component.tsx
@@ -24,13 +24,37 @@ export const Pagination = ({ total, onPageChange }: PaginationProps) => {
         <em>elemPerPage: {elemPerPage}</em>
       </div>
       <div>
-        {[...Array(nbPage)]
+        <nav aria-label="Page navigation example">
+          <ul className="pagination">
+            {[...Array(nbPage)]
+              .map((_, i) => i + 1)
+              .map((item) => (
+                <li
+                  key={item}
+                  onClick={() => changePage(item)}
+                  className={`page-item${
+                    currentPage === item ? " active" : ""
+                  }`}
+                >
+                  <button className="page-link">
+                    {currentPage === item ? `-${item}-` : item}
+                  </button>
+                </li>
+              ))}
+            {/* <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+    <li className="page-item"><a className="page-link" href="#">1</a></li>
+    <li className="page-item"><a className="page-link" href="#">2</a></li>
+    <li className="page-item"><a className="page-link" href="#">3</a></li>
+    <li className="page-item"><a className="page-link" href="#">Next</a></li> */}
+          </ul>
+        </nav>
+        {/* {[...Array(nbPage)]
           .map((_, i) => i + 1)
           .map((item) => (
             <button key={item} onClick={() => changePage(item)}>
               {currentPage === item ? `-${item}-` : item}
             </button>
-          ))}
+          ))} */}
       </div>
     </>
   );
