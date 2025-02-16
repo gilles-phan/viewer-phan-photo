efbd057f918adfe1de4222372a62d0cfd56f4d95
Gilles Phan <phan.gilles@gmail.com>
2025-02-16 21:39:05 +0100

    - update script to accept param "path" to list all files on specific folder

---
+++ backend/list.php
@@ -0,0 +1,24 @@
+<?php
+    header('Content-Type: application/json');
+
+    // Récupère le chemin passé en paramètre (ou "./" par défaut)
+    $dir = isset($_GET['path']) ? realpath($_GET['path']) : "./";
+
+    // Vérifie si le chemin est valide et est bien un dossier
+    if ($dir && is_dir($dir)) {
+        $list = [];
+
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+                if ($file !== "." && $file !== "..") {
+                    $list[] = $file;
+                }
+            }
+            closedir($dh);
+        }
+
+        echo json_encode(["status" => "success", "files" => $list]);
+    } else {
+        echo json_encode(["status" => "error", "message" => "Répertoire invalide"]);
+    }
+?>
