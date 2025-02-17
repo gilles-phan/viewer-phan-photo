e2bf42d8472c52776a45a77a7aa0f37db865aa97
Gilles Phan <phan.gilles@gmail.com>
2025-02-17 13:35:58 +0100

    - add text for Home

--- src/components/Home/Home.component.tsx
+++ src/components/Home/Home.component.tsx
@@ -1,11 +1,41 @@
+import { Col, Divider, Row, Typography } from "antd";
 import "./Home.scss";
+const { Title, Text } = Typography;
 
 export const Home = () => {
   console.log("home");
 
   return (
-    <div className="home-wrapper">
-      <h1>Accueil</h1>
-    </div>
+    <Row>
+      <Col span={20} offset={2}>
+        <Row>
+          <Col span={24}>
+            <Title style={{ marginTop: 12 }}>
+              Bienvenue sur ma galerie photo ! 🎞️
+            </Title>
+          </Col>
+          <Col span={24}>
+            <Divider />
+          </Col>
+          <Col span={24}>
+            <Text type="secondary">
+              Ici, je partage une sélection de mes plus belles images, capturées
+              avec passion, que ce soit lors des événements sportifs, de séances
+              portrait, ou lors de moments épiques avec les chevaux. Chaque
+              photo raconte une histoire, et j’espère qu’elles sauront toucher
+              quelque chose en vous. N’hésitez pas à explorer, à apprécier, et à
+              partager vos coups de cœur !
+            </Text>
+          </Col>
+          <Col span={24}>
+            <Text type="secondary">
+              N'hésitez pas à me contacter par mail (
+              <em>à phan.gilles@gmail.com</em>) si vous rencontrez un bug ou que
+              vous souhaitez me connseiler des fonctionnalités
+            </Text>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   );
 };
